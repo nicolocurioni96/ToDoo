820e2a900f92d163f334a4a039763f8205295db2
Nicolò Curioni <nicolo.curioni96@gmail.com>
2023-10-08 23:22:52 +0200

    Initial Commit

---
+++ ToDoo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDoo
+//
+//  Created by Nicolò Curioni on 08/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDoo/ToDooApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDooApp.swift
+//  ToDoo
+//
+//  Created by Nicolò Curioni on 08/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
